2fe43983e62e954c4d885a26672aa9dc873dc505
ElieBadari <elie.badari@hotmail.com>
2022-02-17 00:34:08 +0200

    Added spinner and user input

--- app/src/main/java/com/example/csc498x_geeks_love_calculator/MainActivity.java
+++ app/src/main/java/com/example/csc498x_geeks_love_calculator/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.csc498x_geeks_love_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        String[] currencies = new String[]{"", "Java", "HTML","Python",""};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, currencies);
+        spinner.setAdapter(adapter);
     }
 }
