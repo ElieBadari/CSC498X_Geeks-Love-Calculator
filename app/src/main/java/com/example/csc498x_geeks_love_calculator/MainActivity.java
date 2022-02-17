2f5a5296b669e07fc18ebbefba17738cf6035cbe
ElieBadari <elie.badari@hotmail.com>
2022-02-17 10:29:22 +0200

    fixed output mechanics

--- app/src/main/java/com/example/csc498x_geeks_love_calculator/MainActivity.java
+++ app/src/main/java/com/example/csc498x_geeks_love_calculator/MainActivity.java
@@ -20,42 +20,42 @@ public class MainActivity extends AppCompatActivity {
     String[] languages;
     ArrayAdapter<String> adapter;
     ImageView img;
-    TextView output;
-    Random random;
+    TextView status;
+    Random random = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         img = (ImageView) findViewById(R.id.img);
         spinner = (Spinner) findViewById(R.id.lang_sp);
-        languages = new String[]{"", "JavaScript", "HTML", "CSS", "Swift", "C++", "C#", "TypeScript"};
-        adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, languages);
+        languages = new String[]{"JavaScript", "HTML", "CSS", "Swift", "C++", "C#", "TypeScript"};
+        adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, languages);
         spinner.setAdapter(adapter);
+//        status.setText(" ");
         input = (EditText) findViewById(R.id.user);
-        output = (TextView) findViewById(R.id.output);
+        status = (TextView) findViewById(R.id.out_percent);
 
     }
 
     public void match(View view){
         full_name = input.getText().toString();
         img.setTranslationY(-1500);
-
-        int out = (int)(Math.random()*(100-0+1)+0);
-        output.setText(out);
-        if (spinner.getSelectedItem().toString() == languages[1]){
+        int rand = random.nextInt(100) + 1;
+        String message = full_name + " scored " + rand + "%";
+        status.setText(message);
+        if (spinner.getSelectedItem().toString().equalsIgnoreCase(languages[0])){
             img.setImageResource(R.drawable.javascript);
-        }else if (spinner.getSelectedItem().toString() == languages[2]){
+        }else if (spinner.getSelectedItem().toString().equalsIgnoreCase(languages[1])){
             img.setImageResource(R.drawable.html);
-        }else if (spinner.getSelectedItem().toString() == languages[3]){
+        }else if (spinner.getSelectedItem().toString().equalsIgnoreCase(languages[2])){
             img.setImageResource(R.drawable.css);
-        }else if (spinner.getSelectedItem().toString() == languages[4]){
+        }else if (spinner.getSelectedItem().toString().equalsIgnoreCase(languages[3])){
             img.setImageResource(R.drawable.swift);
-        }else if (spinner.getSelectedItem().toString() == languages[5]){
+        }else if (spinner.getSelectedItem().toString().equalsIgnoreCase(languages[4])){
             img.setImageResource(R.drawable.c);
-        }else if (spinner.getSelectedItem().toString() == languages[6]){
+        }else if (spinner.getSelectedItem().toString().equalsIgnoreCase(languages[5])){
             img.setImageResource(R.drawable.csharp);
-        }else if (spinner.getSelectedItem().toString() == languages[7]){
+        }else if (spinner.getSelectedItem().toString().equalsIgnoreCase(languages[6])){
             img.setImageResource(R.drawable.typescript);
         }
         img.animate().translationYBy(1500).rotation(360*10).setDuration(10000);
